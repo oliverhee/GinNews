c975f8c6b567abaca605352e6be50efc8ccd7f05
GHE37 <43193579+oliverhee@users.noreply.github.com>
2022-01-29 14:53:44 -0500

    updated folder

---
+++ TinNews/app/src/main/java/com/laioffer/tinnews/ui/save/SaveViewModel.java
@@ -0,0 +1,27 @@
+package com.laioffer.tinnews.ui.save;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.ViewModel;
+
+import com.laioffer.tinnews.model.Article;
+import com.laioffer.tinnews.repository.NewsRepository;
+
+import java.util.List;
+
+public class SaveViewModel extends ViewModel {
+    public SaveViewModel(NewsRepository repository) {
+        this.repository = repository;
+    }
+
+    private final NewsRepository repository;
+
+    public LiveData<List<Article>> getAllSavedArticles() {
+        return repository.getAllSavedArticles();
+    }
+
+    public void deleteSavedArticle(Article article) {
+        repository.deleteSavedArticle(article);
+    }
+
+
+}
